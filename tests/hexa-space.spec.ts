e2fff8c0943425a7cbefa75d790672b5278a67ca
Sophia Cook <sophia.cook@codecentric.de>
2024-03-20 17:42:33 +0100

    add base url to playwright config

--- tests/hexa-space.spec.ts
+++ tests/hexa-space.spec.ts
@@ -1,14 +1,14 @@
 import {test, expect} from '@playwright/test';
 
 test('has title', async ({page}) => {
-    await page.goto('http://127.0.0.1:3000');
+    await page.goto('/');
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Hexa Space App/);
 });
 
 test('get space ship link', async ({page}) => {
-    await page.goto('http://127.0.0.1:3000');
+    await page.goto('/');
 
     // Click the get started link.
     await page.getByRole('link', {name: 'space ships'}).click();
